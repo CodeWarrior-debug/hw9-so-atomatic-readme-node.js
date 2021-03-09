5c790a62505a55e4c11a11bafd4b0126d3801f3f
James Jordan <jpjordan1987@gmail.com>
2021-03-08 20:41:34 -0500

    moving files around for functioning

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,9 +1,6 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 
-
-
-
 function renderLicenseBadge(license) {
   licenseObj = {
     MIT:"[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)",
@@ -12,59 +9,32 @@ function renderLicenseBadge(license) {
   return licenseObj[license];
 }
 
-
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
   linkObj = {
-    MIT: "(https://opensource.org/licenses/MIT)]",
-    Apache: "(https://opensource.org/licenses/Apache-2.0)]",
+    MIT: "(https://opensource.org/licenses/MIT)",
+    Apache: "(https://opensource.org/licenses/Apache-2.0)",
   }
-
   return linkObj[license];
 }
-
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-
-//   const fs = require('fs') 
-  
-// fs.readFile('Input.txt', (err, data) => { 
-//     if (err) throw err; 
-  
-//     console.log(data.toString()); 
-
-
-
-
-            let myfile = 
-            let fr=new FileReader(); 
-            fr.onload=function(){
-
-                document.getElementById('output') 
-                        .textContent=fr.result; 
-            } 
-              
-            fr.readAsText(this.files[0]); 
-        }) 
-
-    let var fr=new FileReader(); 
-            fr.onload=function(){ 
-                document.getElementById('output') 
-                        .textContent=fr.result; 
-            } 
-              
-            fr.readAsText(this.files[0]);  
-    </script> 
-}
+  const fs = require('fs'); 
+ fs.readFile('Apache.txt', 'utf-8', (err, data) => { 
+    if (err) throw err;
+    return data; 
+        })
+      }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   
-  return `My license is a --- ${data.license}
+  return `
   ${renderLicenseBadge(data.license)}${renderLicenseLink(data.license)}
+  ${renderLicenseSection(data.license)}
 `
 ;
 }
--- index.js
+++ index.js
@@ -2,60 +2,75 @@
 
 const inquirer = require("inquirer");
 const fs = require('fs');
-const generateMarkdown = require("./utils/generateMarkdown");
+const generateMarkdown = require("generateMarkdown");
 
 // array of questions for user input
 const questions = [
-    'Description: Please describe this project?','Outline: Provide a Table of Contents?', 'Installation: What are the required steps to install?', 'Usage: What is it supposed to be used for?', 'License: What license will you select?', 'Contributing: How can others contribute?', 'Tests: RESEARCH WHAT THE HECK THIS IS', 'QUESTIONS: RESEARCH ALSO', 'Pick your license below:'
+    'Description: Please describe this project?', 'Outline: Provide a Table of Contents?', 'Title?', 'Installation: What are the required steps to install?', 'Usage: What is it supposed to be used for?', 'Contributing: How can others contribute?', 'Tests: RESEARCH WHAT THE HECK THIS IS', 'QUESTIONS: RESEARCH ALSO', 'Pick your license below:'
 ];
 
 // TODO: Create an array of questions for user input
-const names = ['description', 'toc','installation', 'usage', 'license', 'contributing', 'tests', 'questions','license'];
+const names = ['description', 'toc', 'title', 'installation', 'usage', 'contributing', 'tests', 'questions', 'license'];
+
+ //     1           2       3           4              5                   7              8        9           10
+// ['description', 'toc', 'title', 'installation', 'usage', , 'contributing', 'tests', 'questions', 'license'];
 
 // TODO: Create a function to write README file*********************status: test ********************
 //function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
 function init() {
-    
+
     inquirer
-    .prompt(
-        [
-            {type: 'input', message: questions[0], name: names[0]},
-            {type: 'input', message: questions[1], name: names[1]},
-            // {type: 'input', message: questions[2], name: names[2]},
-            // {type: 'input', message: questions[3], name: names[3]},
-            // {type: 'input', message: questions[4], name: names[4]},
-            // {type: 'input', message: questions[5], name: names[5]},
-            // {type: 'input', message: questions[6], name: names[6]},
-            // {type: 'input', message: questions[7], name: names[7]},
-            {type: 'rawlist', message: questions[8], name: names[8],choices:['MIT','Apache']},
-        ])
-    .then((answers) => {
-        
-        // const myAnswer = `This is working! ${names[1]} now`; // [answer.description];
+        .prompt(
+            [{
+                    type: 'input',
+                    message: questions[0],
+                    name: names[0]
+                },
+                {
+                    type: 'input',
+                    message: questions[1],
+                    name: names[1]
+                },
+                {type: 'input', message: questions[2], name: names[2]},
+                {type: 'input', message: questions[3], name: names[3]},
+                {type: 'input', message: questions[4], name: names[4]},
+                {type: 'input', message: questions[5], name: names[5]},
+                {type: 'input', message: questions[6], name: names[6]},
+                {type: 'input', message: questions[7], name: names[7]},
+                {
+                    type: 'rawlist',
+                    message: questions[8],
+                    name: names[8],
+                    choices: ['MIT', 'Apache']
+                },
+            ])
+        .then((answers) => {
 
-        
-        fs.writeFile('new.md',generateMarkdown(answers), function(err){
-            if (err) return console.log(err);
-        // fs.writeFile('new.md',generatemd(myAnswer), function(err){
-        //     if (err) return console.log(err);
+            // const myAnswer = `This is working! ${names[1]} now`; // [answer.description];
+
+            fs.writeFile('new.md', generateMarkdown(answers), function (err) {
+                if (err) return console.log(err);
+                // fs.writeFile('new.md',generatemd(myAnswer), function(err){
+                //     if (err) return console.log(err);
+            })
         })
-    })}
+}
 
 
 
-    //{
-        // const myAnswer = [answer.description];
-        // fs.writeFile('new.md',generatemd(myAnswer), function(err){
-        //     if (err) return console.log(err);
-        // })
+//{
+// const myAnswer = [answer.description];
+// fs.writeFile('new.md',generatemd(myAnswer), function(err){
+//     if (err) return console.log(err);
+// })
 
 // fs.writeFile('new.md',generatemd(answer), function(err){
 //     if (err) return console.log(err);
-        //['Description', 'TOC','Installation', 'Usage', 'License', 'Contributing', 'Tests', 'Questions'];
+//['Description', 'TOC','Installation', 'Usage', 'License', 'Contributing', 'Tests', 'Questions'];
 
-        // console.log(answer.names[1]);
+// console.log(answer.names[1]);
 
 // console.log("Hello " + answer.name);
 // console.log("Hello " + answer.quest);
